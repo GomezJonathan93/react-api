31bfe287875cd157de87df6bbab43127b1e3774d
Jonathan Gomez <jurielgomez_1993@hotmail.com>
2022-10-17 16:40:56 -0300

    primer commit

---
+++ OneDrive/Escritorio/ReactJs/Entrega Proyecto Final I/proyectofinaluno/src/components/cardwidget.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CartWidget(){
+    return(
+        <a href="" target="_blank"><i class="fa-solid fa-cart-shopping"></i></a>
+    )
+}
+
+export default CartWidget
---
+++ OneDrive/Escritorio/ReactJs/Entrega Proyecto Final I/proyectofinaluno/src/components/itemlistcontainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ListContainer(prop){
+    return(
+        <p>¡Hola, {prop.nombre} {prop.campeon}!</p>
+    )
+}
+
+export default ListContainer
